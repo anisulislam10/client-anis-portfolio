bbe676e885c65fd76358c4f2b4cb802c4c338162
Anisul Islam <anis.inbox10@gmail.com>
2025-05-23 17:31:19 +0500

    minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,16 +9,8 @@ import Footer from '../components/Footer';
 import Shop_homepage from '../components/shop/Shop_homepage';
 
 const Home = () => {
-
   return (
-
     <>
-      <iframe
-  src="https://custom-gpt-backend-six.vercel.app/api/chatbot/6830632e1060b3f16ac6f8a1/683062a5343a736eb18cb5e3?domain=https%3A%2F%2Fanisdev.vercel.app"
-  style={{ width: '400px', height: '600px', border: 'none', position: 'fixed', bottom: '20px', right: '20px' }}
-  allowTransparency="true"
-  title="Chatbot"
-/>
       <Header />
       <main>
         <Hero />
