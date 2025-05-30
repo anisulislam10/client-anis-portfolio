52997f9bc3a4a2de43b6fa44d3fb9a213d133f0d
Anisul Islam <anis.inbox10@gmail.com>
2025-05-30 19:56:12 +0500

    added script for chatbot

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AllBlogs from './components/AllBlogs';
@@ -8,9 +9,41 @@ import BlogPostPage from './components/BlogPostPage';
 import Shop_homepage from './components/shop/Shop_homepage';
 import Shop_page from './pages/Shop/Shop_page';
 import AddToCart from './components/shop/AddToCart';
+import CheckOut from './components/shop/CheckOut';
+
 // import ProtectedRoute from './components/Admin/ProtectedRoute';
 
 function App() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.src = 'https://custom-gpt-backend-sigma.vercel.app/api/chatbot/script.js';
+    script.async = true;
+
+    script.onload = () => {
+      window.ChatbotConfig = {
+        flowId: "6839c58ce7a8f2f2e652195d",
+        userId: "6839bb594e91b60282f411bb",
+        websiteDomain: "https://anisdev.vercel.app",
+        position: "bottom-right",
+        theme: {
+          primary: "#6366f1",
+          secondary: "#f59e0b",
+          background: "#ffffff",
+          text: "#1f2937"
+        }
+      };
+      if (window.initChatbot) {
+        window.initChatbot();
+      }
+    };
+
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script); // optional cleanup
+    };
+  }, []);
+
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
@@ -22,16 +55,10 @@ function App() {
           <Route path="/admin" element={<Login />} />
           <Route path="/super-admin/dashboard" element={<Dashboard />} />
 
-{/* shop routes */}
-
-<Route path="/shop" element={<Shop_page />} />
-<Route path="/shop/cart"  element={<AddToCart />} />
-
-
-
-
-
-
+          {/* shop routes */}
+          <Route path="/shop" element={<Shop_page />} />
+          <Route path="/shop/cart" element={<AddToCart />} />
+          <Route path="/shop/cart/checout" element={<CheckOut />} />
         </Routes>
       </div>
     </Router>
