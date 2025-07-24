1fe2d8e25cf4f3ebf45b8074731cfd8c307c5176
Anisul Islam <anis.inbox10@gmail.com>
2025-07-24 23:26:44 +0500

    new

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './Redux/store';
 import App from './App';
 import './index.css';
+import { Analytics } from "@vercel/analytics/next"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
